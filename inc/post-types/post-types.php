877898bed3e420538ef11e6e14cd9dde7d5b6280
Mahbub <mmhasaneee@gmail.com>
2019-01-21 23:47:18 +0600

    Reorder admin menus

--- inc/post-types/post-types.php
+++ inc/post-types/post-types.php
@@ -41,3 +41,51 @@ function easl_change_pt_labels_post($labels) {
 	return $labels;
 }
 add_filter('post_type_labels_post', 'easl_change_pt_labels_post', 10);
+
+function easl_admin_menu_change() {
+	global $menu, $submenu, $pagenow, $title;
+	// Rename menu
+	$to_rename = array(
+		'revslider' => 'Homepage Sliders'
+	);
+	foreach ( $menu as $id => $data ) {
+		if ( isset( $to_rename[$data[ 2 ]] ) ) {
+			$menu[ $id ][0] = $to_rename[$data[ 2 ]];
+			$menu[ $id ][3] = $to_rename[$data[ 2 ]];
+		}
+	}
+}
+add_action( 'admin_menu', 'easl_admin_menu_change', 200 );
+
+add_filter( 'custom_menu_order', '__return_true' );
+function easl_menu_order($menu_order) {
+	$separator1_position	 = array_search( 'separator1', $menu_order );
+	if('false' === $separator1_position) {
+		$separator1_position = 1;
+	}
+	$front_end_menus = array(
+		'edit.php?post_type=annual_reports', // Annual Reports
+		'edit.php?post_type=award', // Awards
+		'edit.php?post_type=event', // Events
+		'edit.php?post_type=fellowship', // Fellowship
+		'revslider', // Home page sliders
+		'edit.php?post_type=ilc', // ILC
+		'edit.php?post_type=associations', // National Associations
+		'edit.php',// News/posts
+		'edit.php?post_type=newsletter', // Newsletters
+		'edit.php?post_type=page', // Pages
+		'edit.php?post_type=publication', // Publications
+		'edit.php?post_type=staff', // People
+		'edit.php?post_type=slide_decks', // Slide Decks
+	);
+	foreach ($front_end_menus as $menu_id) {
+		$position = array_search($menu_id, $menu_order);
+		if('false' != $position) {
+			unset($menu_order[$position]);
+		}
+	}
+	array_splice( $menu_order, $separator1_position, 0, $front_end_menus );
+	return $menu_order;
+
+}
+add_filter( 'menu_order', 'easl_menu_order' );
