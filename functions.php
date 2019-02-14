d16db74b4e91f96b257ba8f0e5e27a64729398ee
Mahbub <mmhasaneee@gmail.com>
2019-02-14 16:25:30 +0600

    Load parenst style through wp_enqueue_scripts

--- functions.php
+++ functions.php
@@ -23,6 +23,17 @@ function easl_theme_setup(){
 }
 add_action( 'after_setup_theme', 'easl_theme_setup' );
 
+function total_child_enqueue_parent_theme_style() {
+
+	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update your theme)
+	$theme   = wp_get_theme( 'Total' );
+	$version = $theme->get( 'Version' );
+
+	// Load the stylesheet
+	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array(), $version );
+
+}
+add_action( 'wp_enqueue_scripts', 'total_child_enqueue_parent_theme_style' );
 
 function easl_custom_scripts(){
 	wp_enqueue_script('jquery');
