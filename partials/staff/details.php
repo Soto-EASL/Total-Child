f50ff6554a3b2cdd46853bef07471feda1ab1fdc
Mahbub <mmhasaneee@gmail.com>
2019-02-17 17:58:45 +0600

    Revamped staff details template

--- partials/staff/details.php
+++ partials/staff/details.php
@@ -12,42 +12,29 @@ if ( has_post_thumbnail( $post->ID ) ) {
 		$img_path = $image[ 0 ];
 	}
 }
+if(!$img_path){
+    $img_path = get_stylesheet_directory_uri() . '/images/default-avatar.png';
+}
 $terms = get_the_terms( $post->ID, 'staff_category' );
 ?>
 <div class="profile-block-wrapper">
-	<div class="vc_row wpb_row vc_row-fluid">
-		<div class="wpb_column vc_column_container vc_col-sm-12">
-			<div class="vc_column-inner custom-margin-0">
-				<div class="wpb_wrapper">
-					<div class="easl-member-profile">
-						<div class="easl-member-profile-header vc_clearfix">
-							<div class="sp-thumb">
-								<img style="max-width: 250px;" alt='' src='<?php echo $img_path ? $img_path : get_theme_file_uri( 'images/default-avatar.png' ); ?>' class='avatar avatar-121 photo'/>
-							</div>
-							<div class="sp-content">
-								<h2 class="easl-member-profile-name"><?php the_title(); ?></h2>
-								<?php if ( $terms ): ?>
-									<?php foreach ( $terms as $term ): ?>
-										<h4 class="easl-member-profile-position"><?php echo $term->name; ?></h4>
-									<?php endforeach; ?>
-								<?php endif; ?>
-								<p class="sp-excerpt"><?php echo $post->post_excerpt ? $post->post_excerpt : ''; ?></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-
-	<div class="vc_row wpb_row vc_row-fluid">
-		<div class="wpb_column vc_column_container vc_col-sm-12">
-			<div class="vc_column-inner custom-margin">
-				<div class="wpb_wrapper">
-					<div class="easl-member-profile-description vc_clearfix"><?php echo $post->post_content; ?></div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="easl-staff-details-top">
+        <div class="easl-staff-details-thumb">
+            <img src="<?php echo $img_path; ?>" alt="">
+        </div>
+        <div class="easl-staff-details-title-contact">
+            <h2><?php the_title(); ?></h2>
+	        <?php if ( $terms ): ?>
+		        <?php foreach ( $terms as $term ): ?>
+                    <h4 class="easl-staff-details-position"><?php echo $term->name; ?></h4>
+		        <?php endforeach; ?>
+	        <?php endif; ?>
+            <?php if(has_excerpt()): ?>
+            <div class="easl-staff-details-excerpt"><?php the_excerpt(); ?></div>
+            <?php endif; ?>
+        </div>
+    </div>
+    <div class="easl-staff-details-bio">
+        <?php the_content(); ?>
+    </div>
 </div>
