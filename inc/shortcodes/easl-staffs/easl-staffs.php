189859032f49f1169884c9df95d54dae6b516217
Mahbub <mmhasaneee@gmail.com>
2019-05-24 17:50:00 +0600

    Before creating new brances for live

--- inc/shortcodes/easl-staffs/easl-staffs.php
+++ inc/shortcodes/easl-staffs/easl-staffs.php
@@ -16,7 +16,7 @@ if ( class_exists( 'WPBakeryShortCode' ) ) {
 		public function string_to_array( $value ) {
 
 			// Return if value is empty
-			if ( !$value ) {
+			if ( ! $value ) {
 				return;
 			}
 
@@ -47,75 +47,78 @@ if ( class_exists( 'WPBakeryShortCode' ) ) {
 			if ( empty( $cats_ids ) ) {
 				return '';
 			}
-			if ( !in_array( $cat_relation, array( 'IN', 'NOT IN', 'AND', 'EXISTS' and 'NOT EXISTS' ) ) ) {
+			if ( ! in_array( $cat_relation, array( 'IN', 'NOT IN', 'AND', 'EXISTS' and 'NOT EXISTS' ) ) ) {
 				$cat_relation = 'AND';
 			}
 			$tax_query = array(
-				'taxonomy'	 => 'staff_category',
-				'field'		 => 'term_id',
-				'terms'		 => $cats_ids,
+				'taxonomy' => 'staff_category',
+				'field'    => 'term_id',
+				'terms'    => $cats_ids,
 			);
 			if ( count( $cats_ids ) > 1 ) {
-				$tax_query[ 'operator' ] = $cat_relation;
+				$tax_query['operator'] = $cat_relation;
 			}
+
 			return array( $tax_query );
 		}
 
 		public function staff_has_details( $staff_id ) {
 			$staff_post = get_post( $staff_id );
-			if ( !$staff_post ) {
+			if ( ! $staff_post ) {
 				return false;
 			}
-			if ( !trim( $staff_post->post_content ) ) {
+			if ( ! trim( $staff_post->post_content ) ) {
 				return false;
 			}
+
 			return true;
 		}
 
-		public function get_staff_profile_thumb($id) {
-			$attachment_id = get_post_thumbnail_id($id);
-			if(!$attachment_id){
+		public function get_staff_profile_thumb( $id ) {
+			$attachment_id = get_post_thumbnail_id( $id );
+			if ( ! $attachment_id ) {
 				return '';
 			}
-			$attachment_src = wp_get_attachment_image_src($attachment_id, 'staff_grid');
-			if(!$attachment_src) {
+			$attachment_src = wp_get_attachment_image_src( $attachment_id, 'staff_grid' );
+			if ( ! $attachment_src ) {
 				return '';
 			}
-			return sprintf('<img src="%s" alt="" width="254" height="254"/>', $attachment_src[0]);
+
+			return sprintf( '<img src="%s" alt="" width="254" height="254"/>', $attachment_src[0] );
 		}
 
 		public function parse_info_template( $template, $id ) {
 			$template = trim( $template );
-			if ( !$template ) {
+			if ( ! $template ) {
 				return '';
 			}
-			$parsed_output	 = '';
-			$staff_post		 = get_post( $id );
-			if ( !$staff_post ) {
+			$parsed_output = '';
+			$staff_post    = get_post( $id );
+			if ( ! $staff_post ) {
 				return '';
 			}
 			if ( function_exists( 'get_field' ) ) {
-				$telephone	 = get_field( 'telephone', $id );
-				$fax		 = get_field( 'fax', $id );
-				$email		 = get_field( 'email', $id );
-				$city		 = get_field( 'city', $id );
-				$country	 = get_field( 'country', $id );
-				$email		 = get_field( 'email', $id );
-				$job_title		 = get_field( 'job_title', $id );
-				$easl_posiiton		 = get_field( 'easl_posiiton', $id );
+				$telephone     = get_field( 'telephone', $id );
+				$fax           = get_field( 'fax', $id );
+				$email         = get_field( 'email', $id );
+				$city          = get_field( 'city', $id );
+				$country       = get_field( 'country', $id );
+				$email         = get_field( 'email', $id );
+				$job_title     = get_field( 'job_title', $id );
+				$easl_position = get_field( 'easl_position', $id );
 			} else {
-				$telephone	 = get_post_meta( $id, 'telephone', true );
-				$fax		 = get_post_meta( $id, 'fax', true );
-				$email		 = get_post_meta( $id, 'email', true );
-				$city		 = get_post_meta( $id, 'city', true );
-				$country	 = get_post_meta( $id, 'country', true );
-				$email		 = get_post_meta( $id, 'email', true );
-				$job_title		 = get_post_meta( $id, 'job_title', true );
-				$easl_posiiton		 = get_post_meta( $id, 'easl_posiiton', true );
+				$telephone     = get_post_meta( $id, 'telephone', true );
+				$fax           = get_post_meta( $id, 'fax', true );
+				$email         = get_post_meta( $id, 'email', true );
+				$city          = get_post_meta( $id, 'city', true );
+				$country       = get_post_meta( $id, 'country', true );
+				$email         = get_post_meta( $id, 'email', true );
+				$job_title     = get_post_meta( $id, 'job_title', true );
+				$easl_position = get_post_meta( $id, 'easl_position', true );
 			}
-			$excerpt		 = $staff_post->post_excerpt;
-			$name			 = get_the_title( $id );
-			$contact_info	 = '';
+			$excerpt      = $staff_post->post_excerpt;
+			$name         = get_the_title( $id );
+			$contact_info = '';
 
 			if ( $telephone ) {
 				$contact_info .= sprintf( '<p class="easl-staff-telephone"><a href="tel:%s">%s</a></p>', $telephone, $telephone );
@@ -127,20 +130,21 @@ if ( class_exists( 'WPBakeryShortCode' ) ) {
 				$contact_info .= sprintf( '<p class="easl-staff-email"><a href="mailto:%s">%s</a></p>', $email, $email );
 			}
 
-			$replaces = array(
-				'{{telephone}}'		 => $telephone,
-				'{{fax}}'			 => $fax,
-				'{{email}}'			 => $email,
-				'{{contact_info}}'	 => $contact_info,
-				'{{city}}'			 => $city,
-				'{{country}}'		 => $country,
-				'{{excerpt}}'		 => $excerpt,
-				'{{name}}'			 => $name,
-				'{{job_title}}'		=> $job_title,
-				'{{easl_posiiton}}'		=> $easl_posiiton,
+			$replaces      = array(
+				'{{telephone}}'     => $telephone,
+				'{{fax}}'           => $fax,
+				'{{email}}'         => $email,
+				'{{contact_info}}'  => $contact_info,
+				'{{city}}'          => $city,
+				'{{country}}'       => $country,
+				'{{excerpt}}'       => $excerpt,
+				'{{name}}'          => $name,
+				'{{job_title}}'     => $job_title,
+				'{{easl_position}}' => $easl_position,
 			);
-			$parsed_output = str_replace(array_keys($replaces), array_values($replaces), $template);
-			return trim($parsed_output);
+			$parsed_output = str_replace( array_keys( $replaces ), array_values( $replaces ), $template );
+
+			return trim( $parsed_output );
 		}
 
 		public static function get_staff_fields() {
@@ -170,5 +174,4 @@ if ( is_admin() ) {
 };
 
 
-
-vc_lean_map('easl_staffs', null, get_theme_file_path('inc/shortcodes/easl-staffs/map.php'));
+vc_lean_map( 'easl_staffs', null, get_theme_file_path( 'inc/shortcodes/easl-staffs/map.php' ) );
